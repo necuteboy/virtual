a553ba10b6e83f2e49d48bb2163a8d0346883aee
necuteboy <89706104+necuteboy@users.noreply.github.com>
2021-10-26 01:06:10 +0500

    Update vm.js

--- vm.js
+++ vm.js
@@ -1,12 +1,23 @@
 let fs=require('fs');
 let ram=new Array();
-inputData=fs.readFileSync('input.jss').toString();
+inputData=fs.readFileSync('fact.jss').toString();
 ram=inputData.split(/\s+/);
-let fact=1;
-let t=0;
-let ind1=0;
-let ind2=0;
+let zr;
 var readlineSync = require('readline-sync');
+function  goto(name){
+	let t=0
+	while(ram[t]!=name){
+		t++
+	}
+	return t;
+}
+function godown(name,n){
+	let t=n+1;
+	while(ram[t]!=name){
+		t++
+	}
+	return t;
+}
 for (let i=0; i<ram.length;) {
 	if (ram[i]=="input"){
 		ram[Number(ram[i+1])]=readlineSync.question();
@@ -28,71 +39,55 @@ for (let i=0; i<ram.length;) {
 		ram[Number(ram[i + 3])] = ram[Number(ram[i+1])] * ram[Number(ram[i+2])];
 		i++
 	}
-	else if (ram[i]=="node"){
-		if (ram[Number(ram[i+1])]==0 || ram[Number(ram[i+2])]==0){
-			ram[Number(ram[i+3])]=1;
-			i++
+	else if (ram[i]=="compare"){
+		if (ram[Number(ram[i+1])]-ram[Number(ram[i+2])]<0){
+			zr=-1;
 		}
-		else if (ram[Number(ram[i+1])]!=ram[Number(ram[i+2])]) {
-			if (ind1==0 && ind2==0) {
-				ind1 = Number(ram[Number(ram[i + 1])]);
-				ind2 = Number(ram[Number(ram[i + 2])]);
-			}
-			else {
-				ram[Number(ram[i+1])]=Math.abs(ram[Number(ram[i+1])]);
-				ram[Number(ram[i+2])]=Math.abs(ram[Number(ram[i+2])]);
-				//console.log(ram[Number(ram[i+1])],ram[Number(ram[i+2])]);
-				if (ram[Number(ram[i + 1])] > ram[Number(ram[i + 2])]) {
-					ram[Number(ram[i + 1])] -= ram[Number(ram[i + 2])];
-					if (ram[Number(ram[i + 1])] < 0 || ram[Number(ram[i + 2])] < 0) {
-						ram[Number(ram[i + 3])] = 1;
-						ram[Number(ram[i + 1])]=ind1;
-						ram[Number(ram[i + 2])]=ind2;
-						i++
-					}
-					else {
-						i--;
-					}
-				}
-				else {
-					ram[Number(ram[i + 2])] -= ram[Number(ram[i + 1])]
-					if (ram[Number(ram[i + 1])] < 0 || ram[Number(ram[i + 2])] < 0) {
-						ram[Number(ram[i + 3])] = 1;
-						ram[Number(ram[i + 1])]=ind1;
-						ram[Number(ram[i + 2])]=ind2;
-						i++
-					}
-					else {
-						i--;
-					}
-				}
-			}
+		else if (ram[Number(ram[i+1])]-ram[Number(ram[i+2])]>0){
+			zr=0;
+		}
+		else {
+			zr=1;
+		}
+		i+=3;
+	}
+	else if (ram[i]=="point"){
+		i+=2;
+	}
+	else if (ram[i]=="je"){
+		if (zr==1) {
+			i = Number(godown(ram[i + 1], i + 1))
 		}
 		else{
-			ram[Number(ram[i+3])]=Math.abs(Number(ram[Number(ram[i+2])]));
-			ram[Number(ram[i + 1])]=ind1;
-			ram[Number(ram[i + 2])]=ind2;
-			i++
+			i+=2;
 		}
 	}
-	else if (ram[i]=="fact") {
-		if (ram[Number(ram[i + 1])] < 0) {
-			ram[Number(ram[i + 2])]=NaN;
-			i++
-		} else if (ram[Number(ram[i + 1])] == 0) {
-			ram[Number(ram[i + 2])] = 1;
-			i++
-		} else {
-			if (ram[Number(ram[i + 1])] - t != 0) {
-				fact *= ram[Number(ram[i + 1])] - t;
-				t += 1;
-				i--
-			} else {
-				ram[Number(ram[i + 2])] = fact;
-				fact = 1;
-				i++
-			}
+	else if (ram[i]=="jl"){
+		if (zr==-1){
+			i = Number(godown(ram[i + 1], i + 1))
 		}
+		else{
+			i+=2;
+		}
+	}
+	else if (ram[i]=="jm"){
+		if (zr==0){
+			i = Number(godown(ram[i + 1], i + 1))
+		}
+		else{
+			i+=2;
+		}
+	}
+	else if (ram[i]=="move"){
+		i=Number(goto(ram[i+1]));
+	}
+	else if (ram[i]=="new"){
+		ram[Number(ram[i+1])]=ram[i+2];
+		i++
+	}
+	else if (ram[i]=="set"){
+		ram[Number(ram[i+1])]=ram[Number(ram[i+2])];
+		i++
 	}
 	else{
 		i++;
